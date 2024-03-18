1e0c1e323cf5f567217110ad60e120d60e94b685
AngeloRSantos <angelo.s@chirpish.co>
2024-03-18 19:01:53 +0800

    Feat: Category Repository Class

--- src/main/java/com/LuhxEn/PointOfSaleBackEnd/category/CategoryRepository.java
+++ src/main/java/com/LuhxEn/PointOfSaleBackEnd/category/CategoryRepository.java
@@ -1,2 +1,10 @@
-package com.LuhxEn.PointOfSaleBackEnd.category;public interface CategoryRepository {
+package com.LuhxEn.PointOfSaleBackEnd.category;
+
+import com.LuhxEn.PointOfSaleBackEnd.business.Business;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+//	List<Category> findByBusiness(Business business);
 }
