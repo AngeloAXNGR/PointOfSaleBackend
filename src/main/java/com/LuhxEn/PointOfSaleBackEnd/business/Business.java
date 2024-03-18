b71af0565a6cff9cf6a6e0ef5b6d5693332ba485
AngeloRSantos <angelo.s@chirpish.co>
2024-03-18 18:57:34 +0800

    Refactor: Add Products and Categories Properties on Business Entity

--- src/main/java/com/LuhxEn/PointOfSaleBackEnd/business/Business.java
+++ src/main/java/com/LuhxEn/PointOfSaleBackEnd/business/Business.java
@@ -1,14 +1,22 @@
 package com.LuhxEn.PointOfSaleBackEnd.business;
 
 
+import com.LuhxEn.PointOfSaleBackEnd.category.Category;
+import com.LuhxEn.PointOfSaleBackEnd.product.Product;
 import com.LuhxEn.PointOfSaleBackEnd.user.User;
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Data
 @Entity
@@ -23,6 +31,14 @@ public class Business {
 	private String address;
 	private String contactNumber;
 
+
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+	@JoinColumn(name = "business_id")
+	private Set<Category> categories = new HashSet<>();
+
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+	@JoinColumn(name = "business_id")
+	private Set<Product> products = new HashSet<>();
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_id")
