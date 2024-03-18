b17d9f913d6c2189113bbdcb6115a1931e16da39
AngeloRSantos <angelo.s@chirpish.co>
2024-03-18 19:01:01 +0800

    Feat: Category Entity Class

--- src/main/java/com/LuhxEn/PointOfSaleBackEnd/category/Category.java
+++ src/main/java/com/LuhxEn/PointOfSaleBackEnd/category/Category.java
@@ -1,2 +1,32 @@
-package com.LuhxEn.PointOfSaleBackEnd.category;public class Category {
+package com.LuhxEn.PointOfSaleBackEnd.category;
+
+import com.LuhxEn.PointOfSaleBackEnd.business.Business;
+import com.LuhxEn.PointOfSaleBackEnd.product.Product;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String categoryName;
+
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+	@JoinColumn(name = "category_id")
+	private Set<Product> products = new HashSet<>();
 }
