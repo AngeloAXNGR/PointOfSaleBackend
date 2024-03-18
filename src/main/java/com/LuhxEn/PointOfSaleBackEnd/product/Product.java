488656774414297ddbaa3c965612da2c0228a33d
AngeloRSantos <angelo.s@chirpish.co>
2024-03-18 19:03:06 +0800

    Feat: Product Entity Class

--- src/main/java/com/LuhxEn/PointOfSaleBackEnd/product/Product.java
+++ src/main/java/com/LuhxEn/PointOfSaleBackEnd/product/Product.java
@@ -1,2 +1,37 @@
-package com.LuhxEn.PointOfSaleBackEnd.product;public class Product {
+package com.LuhxEn.PointOfSaleBackEnd.product;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String productName;
+
+	// explicitly name as category_id
+	// for @JoinColumn category_id in the Category class
+	@Column(name = "category_id")
+	private Long categoryId;
+
+	private double purchasePrice;
+	private double sellingPrice;
+	private int stock;
+	private int lowStockThreshold;
+
+	// YYYY-MM-DD
+	private Date expiration;
+	private int daysBeforeExpiration;
+
 }
