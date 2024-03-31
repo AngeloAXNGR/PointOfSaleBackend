2996286d19e2232f476008488e905b592d0b88e4
LuhxEn <axngrsantos@gmail.com>
2024-03-31 12:51:57 +0800

    Refactor: Include Transaction Date Property

--- src/main/java/com/LuhxEn/PointOfSaleBackEnd/sale/SaleResponseDTO.java
+++ src/main/java/com/LuhxEn/PointOfSaleBackEnd/sale/SaleResponseDTO.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
 import java.util.List;
 
 @Data
@@ -15,4 +16,5 @@ public class SaleResponseDTO {
 	private Long saleId;
 	private List<ProductListDTO> products;
 	private double grandTotal;
+	private Date transactionDate;
 }
